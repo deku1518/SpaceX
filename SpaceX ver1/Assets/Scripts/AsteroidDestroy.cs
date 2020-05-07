94654114b80dc1212319ba69097ae7decf0c190d
Deku <kartikeyasingh.ks.ks@gmail.com>
2020-05-07 12:27:44 +0530

    Added Score,GameOver and Restart Functionality

--- SpaceX ver1/Assets/Scripts/AsteroidDestroy.cs
+++ SpaceX ver1/Assets/Scripts/AsteroidDestroy.cs
@@ -5,12 +5,35 @@ using UnityEngine;
 public class AsteroidDestroy : MonoBehaviour
 {
 
+    public int scoreValue;
+    private GameController gameController;
+
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+
+        if(gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+    }
+
+
+
+    
     void OnTriggerEnter(Collider other)
+    
     {
         if(other.tag == "Boundary")
         {
             return;
         }
+        if (other.tag == "Player")
+        {
+            gameController.GameOver();
+        }
+        gameController.AddScore(scoreValue);
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
--- SpaceX ver1/Assets/Scripts/GameController.cs
+++ SpaceX ver1/Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -10,12 +11,36 @@ public class GameController : MonoBehaviour
     public float spawnWait;
     public float startWait;
     public float waveWait;
+    public Text scoreText;
+    private int score;
+    public Text restartText;
+    public Text gameoverText;
+    private bool gameOver;
+    private bool restart;
 
     void Start()
     {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameoverText.text = "";
+        score = 0;
+        UpdateScore();
         StartCoroutine(SpawnWaves());
     }
 
+
+
+    void Update()
+    {
+        if(restart)
+        {
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
 
 
    IEnumerator SpawnWaves()
@@ -31,8 +56,36 @@ public class GameController : MonoBehaviour
                 Instantiate(asteroid, spawnPosition, spawnRotation);
             }
             yield return new WaitForSeconds(waveWait);
+
+            if(gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
         }
         
+    }
+
+
+
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+
+    public void GameOver()
+    {
+        gameoverText.text = "Game Over";
+        gameOver = true;
     }
         
 
